6608428bf7e44edb5ff4d9114804e436f9e5d2a8
Lunartyx <dominik@urbanetz.li>
2024-07-26 11:48:45 +0200

    added some icons and changed skill tree

--- src/modules/MySkills.tsx
+++ src/modules/MySkills.tsx
@@ -1,5 +1,10 @@
 import SkillIcon from '../components/SkillIcon';
 
+import skilldata from '../data/Content.json'
+
+const skilllevels = skilldata.pages.myskills.skilllevels
+const skills = skilldata.pages.myskills.skills
+
 // Icon imports
 import linux_icon from '../assets/icons/linux_icon.png';
 import networking_icon from '../assets/icons/network_fundamentals_icon.png'
@@ -14,27 +19,26 @@ import git_icon from '../assets/icons/git_icon.png'
 import pentesting_icon from '../assets/icons/pentesting_icon.png'
 import live_icon from '../assets/icons/live_icon.png'
 
-
 const MySkills = () => {
     return (
         <>
             <div className="grid grid-cols-4 gap-4">
-                <SkillIcon icon={linux_icon} name="Linux" knowledge="Intermediate" />
-                <SkillIcon icon={networking_icon} name="Networking" knowledge="Professional" />
-                <SkillIcon icon={aws_icon} name="AWS" knowledge="Basic" />
-                <SkillIcon icon={azure_icon} name="Azure" knowledge="Basic" />
+                <SkillIcon icon={linux_icon} name={skills[1]} knowledge={skilllevels[3]} />
+                <SkillIcon icon={networking_icon} name={skills[2]} knowledge={skilllevels[4]} />
+                <SkillIcon icon={aws_icon} name={skills[3]} knowledge={skilllevels[1]} />
+                <SkillIcon icon={azure_icon} name={skills[4]} knowledge={skilllevels[1]} />
             </div>
             <div className="grid grid-cols-4 gap-4 mt-8">
-                <SkillIcon icon={js_icon} name="JavaScript" knowledge="Intermediate" />
-                <SkillIcon icon={ts_icon} name="TypeScript" knowledge="Intermediate" />
-                <SkillIcon icon={nodejs_icon} name="NodeJS" knowledge="Intermediate" />
-                <SkillIcon icon={react_icon} name="React" knowledge="Basic" />
+                <SkillIcon icon={js_icon} name={skills[5]} knowledge={skilllevels[3]} />
+                <SkillIcon icon={ts_icon} name={skills[6]} knowledge={skilllevels[2]} />
+                <SkillIcon icon={nodejs_icon} name={skills[7]} knowledge={skilllevels[3]} />
+                <SkillIcon icon={react_icon} name={skills[8]} knowledge={skilllevels[1]} />
             </div>
             <div className="grid grid-cols-4 gap-4 mt-8">
-                <SkillIcon icon={python_icon} name="Python" knowledge="Basic" />
-                <SkillIcon icon={git_icon} name="Git" knowledge="Basic" />
-                <SkillIcon icon={pentesting_icon} name="Pentesting" knowledge="Basic" />
-                <SkillIcon icon={live_icon} name="Music Producing" knowledge="Intermediate" />
+                <SkillIcon icon={python_icon} name={skills[9]} knowledge={skilllevels[1]} />
+                <SkillIcon icon={git_icon} name={skills[10]} knowledge={skilllevels[1]} />
+                <SkillIcon icon={pentesting_icon} name={skills[11]} knowledge={skilllevels[1]} />
+                <SkillIcon icon={live_icon} name={skills[12]} knowledge={skilllevels[2]} />
             </div>
         </>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,16 +62,20 @@ const Home = () => {
                         </div>
                     </div>
                 </section>
-                <section className="snap-start h-screen flex items-center justify-center bg-red-500">
+                <section className="snap-start h-screen flex items-center justify-center">
                     <div className="flex flex-auto">
-                        <div className="basis-1/3">
-                            <img src={portrait}></img>
-                        </div>
-                        <div className="basis-2/3 p-20">
-                            <h1 className="text-6xl">{pagedata.pages.contact.title}</h1>
+                        <div className="w-full p-20 shadow-lg m-8 rounded-3xl">
+
+                            <h1 className="text-6xl">{pagedata.pages.aboutme.title}</h1>
                             <br />
-                            <p className={psize}>{pagedata.pages.contact.content}</p>
-                            <p className={psize}>{pagedata.pages.contact.content2}</p>
+                            <p className={psize}>{pagedata.pages.aboutme.content}</p>
+                            <p className={psize}>{pagedata.pages.aboutme.content2}</p>
+                            <br />
+                            <div className='flex justify-center'>
+                                <div>
+                                    <p className={psize2}>Learn about my skills</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
